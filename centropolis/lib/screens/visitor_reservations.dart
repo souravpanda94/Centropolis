fa50ff25eadfbc538c91999535aa999d2d40b3ba
MondiraHanRiver <83229204+MondiraHanRiver@users.noreply.github.com>
2022-12-21 13:43:27 +0530

    ui modify in VisitorReservationsScreen

--- centropolis/lib/screens/visitor_reservations.dart
+++ centropolis/lib/screens/visitor_reservations.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:centropolis/widgets/common_button_with_icon.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import '../utils/custom_colors.dart';
 import '../widgets/common_app_bar.dart';
 import '../widgets/common_button.dart';
@@ -45,6 +46,43 @@ class _VisitorReservationsScreenState extends State<VisitorReservationsScreen> {
         margin: const EdgeInsets.only(left: 20, right: 20,bottom: 20),
         child: Column(
           children: [
+            Container(
+              margin: const EdgeInsets.only(top: 30),
+              child:  Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children:  [
+                  const Text(
+                    "TODAY",
+                    style: TextStyle(
+                      fontSize: 26,
+                      color: CustomColors.textColor1,
+                      fontFamily: 'SemiBold',
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+
+                  SizedBox(
+                    child: SvgPicture.asset(
+                      'assets/images/ic_right_arrow.svg',
+                      semanticsLabel: 'Back',
+                      width: 15,
+                      height: 15,
+                      alignment: Alignment.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+
+
+
+
+
+
+
+
+
             Expanded(
               child: Align(
                 alignment: FractionalOffset.bottomCenter,
