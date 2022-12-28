6599df05eb211625b44460ab1bc24195a07f0773
SouravHanriver <102669689+SouravHanriver@users.noreply.github.com>
2022-12-28 17:44:15 +0530

    home screen modification

--- centropolis/lib/screens/home.dart
+++ centropolis/lib/screens/home.dart
@@ -54,86 +54,84 @@ class _HomeScreenState extends State<HomeScreen> {
                 ))
           ],
         ),
-        body: ListView(
-          children: [
-            Column(
-              children: [
-                const HomeLogo(),
-                HomePageRow(
-                    title: tr('conference'),
-                    subtitle: tr('conferenceSubTitle'),
-                    image: 'assets/images/conference.png',
-                    min: '02',
-                    max: '04',
-                    name: tr('conferenceRoom')),
-                HomePageRow(
-                    title: tr('visitReservation'),
-                    subtitle: tr('visitor'),
-                    image: 'assets/images/visitor_reservation.png',
-                    min: '01',
-                    max: '08',
-                    name: tr('lobby')),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 60, horizontal: 16),
-                  child: Column(
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            tr('otherReservation'),
-                            style: const TextStyle(
-                                color: CustomColors.textColor1,
-                                fontSize: 24,
-                                fontFamily: 'Pretendard-Regular',
-                                fontWeight: FontWeight.w700),
-                          ),
-                          const SizedBox(
-                            width: 8,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 5),
-                            child: Text(tr('otherReservationSubtitle'),
-                                style: const TextStyle(
-                                    fontFamily: 'Pretendard-Regular',
-                                    color: CustomColors.textColorGrey,
-                                    fontSize: 14)),
-                          )
-                        ],
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.max,
-                        children: [
-                          Expanded(
-                            child: OtherReservationCard(
-                                title: tr('otherReservationCard1')),
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          Expanded(
-                            child: OtherReservationCard(
-                                title: tr('otherReservationCard2')),
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          Expanded(
-                            child: OtherReservationCard(
-                                title: tr('otherReservationCard3')),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                )
-              ],
-            )
-          ],
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const HomeLogo(),
+              HomePageRow(
+                  title: tr('conference'),
+                  subtitle: tr('conferenceSubTitle'),
+                  image: 'assets/images/conference.png',
+                  min: '02',
+                  max: '04',
+                  name: tr('conferenceRoom')),
+              HomePageRow(
+                  title: tr('visitReservation'),
+                  subtitle: tr('visitor'),
+                  image: 'assets/images/visitor_reservation.png',
+                  min: '01',
+                  max: '08',
+                  name: tr('lobby')),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                margin:
+                    const EdgeInsets.symmetric(vertical: 60, horizontal: 16),
+                child: Column(
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          tr('otherReservation'),
+                          style: const TextStyle(
+                              color: CustomColors.textColor1,
+                              fontSize: 24,
+                              fontFamily: 'Pretendard-Regular',
+                              fontWeight: FontWeight.w700),
+                        ),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 5),
+                          child: Text(tr('otherReservationSubtitle'),
+                              style: const TextStyle(
+                                  fontFamily: 'Pretendard-Regular',
+                                  color: CustomColors.textColorGrey,
+                                  fontSize: 14)),
+                        )
+                      ],
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Expanded(
+                          child: OtherReservationCard(
+                              title: tr('otherReservationCard1')),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(
+                          child: OtherReservationCard(
+                              title: tr('otherReservationCard2')),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(
+                          child: OtherReservationCard(
+                              title: tr('otherReservationCard3')),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ));
   }
 }
