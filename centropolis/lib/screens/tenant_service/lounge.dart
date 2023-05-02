9c59c053d2d2818c8698ea188988edb3acda0850
Aditi Dande <aditi@divii.com>
2023-05-02 13:49:54 +0530

    lounge screen

--- centropolis/lib/screens/tenant_service/lounge.dart
+++ centropolis/lib/screens/tenant_service/lounge.dart
@@ -1,10 +1,10 @@
+import 'package:centropolis/widgets/common_button_with_icon.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import '../../utils/custom_colors.dart';
 import '../../utils/utils.dart';
 import '../../widgets/common_app_bar.dart';
 import '../../widgets/common_button.dart';
-
 
 class LoungeScreen extends StatefulWidget {
   const LoungeScreen({super.key});
@@ -14,22 +14,156 @@ class LoungeScreen extends StatefulWidget {
 }
 
 class _LoungeScreenState extends State<LoungeScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        body: Center(
-            child: Text(
-              tr("lounge"),
-              style: const TextStyle(
-                  fontFamily: 'SemiBold',
-                  fontSize: 20,
-                  color: CustomColors.textColor8),
+        body: Stack(
+          children: [
+            SingleChildScrollView(
+                child: Column(
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: Image.asset(
+                    "assets/images/lounge.png",
+                    height: 320,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Container(
+                  color: CustomColors.whiteColor,
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.only(
+                      left: 16, right: 16, top: 24, bottom: 100),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tr("loungeTitle"),
+                        style: const TextStyle(
+                            fontFamily: 'Regular',
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                            color: CustomColors.buttonBackgroundColor),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        tr("loungeTitle"),
+                        style: const TextStyle(
+                            fontFamily: 'Regular',
+                            fontSize: 22,
+                            fontWeight: FontWeight.w600,
+                            color: CustomColors.textColorBlack2),
+                      ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                      Text(
+                        tr("operatingTime"),
+                        style: const TextStyle(
+                            fontFamily: 'Regular',
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                            color: CustomColors.textColor8),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      const Text(
+                        "9 am to 6 pm (weekdays only)",
+                        style: TextStyle(
+                            fontFamily: 'Regular',
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: CustomColors.textColorBlack2),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Text(
+                        tr("availableMenu"),
+                        style: const TextStyle(
+                            fontFamily: 'Regular',
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                            color: CustomColors.textColor8),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      const Text(
+                        "Beverages (coffee - using Haevichi Beans), ade, tea, other beverages), Bakery (bakery/cakes manufactured by Haevichi on the same day)",
+                        style: TextStyle(
+                            fontFamily: 'Regular',
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: CustomColors.textColorBlack2),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Text(
+                        tr("loungeRental"),
+                        style: const TextStyle(
+                            fontFamily: 'Regular',
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                            color: CustomColors.textColor8),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      const Text(
+                        "Rental hours (9:00~22:00)",
+                        style: TextStyle(
+                            fontFamily: 'Regular',
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: CustomColors.textColorBlack2),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Text(
+                        tr("inquiry"),
+                        style: const TextStyle(
+                            fontFamily: 'Regular',
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                            color: CustomColors.textColor8),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      const Text(
+                        "02-6370-5153",
+                        style: TextStyle(
+                            fontFamily: 'Regular',
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: CustomColors.textColorBlack2),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                margin: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
+                child: CommonButton(
+                  onCommonButtonTap: () {},
+                  buttonColor: CustomColors.buttonColor,
+                  buttonName: tr("makeALoungeReservation"),
+                  isIconVisible: true,
+                ),
+              ),
             )
+          ],
         ));
   }
-
-
-
 }
