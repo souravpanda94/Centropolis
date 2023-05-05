93e6fde0042f0c78bbfe0d4d7c645327b487479a
SouravHanriver <102669689+SouravHanriver@users.noreply.github.com>
2023-05-05 13:00:24 +0530

    voc design

--- centropolis/lib/screens/vov_application/air_conditioning.dart
+++ centropolis/lib/screens/vov_application/air_conditioning.dart
@@ -1,10 +1,11 @@
+import 'package:centropolis/screens/vov_application/air_inc_light_list.dart';
+import 'package:centropolis/widgets/voc/voc_common_home.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import '../../utils/custom_colors.dart';
 import '../../utils/utils.dart';
 import '../../widgets/common_app_bar.dart';
 import '../../widgets/common_button.dart';
-
 
 class AirConditioningScreen extends StatefulWidget {
   const AirConditioningScreen({super.key});
@@ -14,22 +15,55 @@ class AirConditioningScreen extends StatefulWidget {
 }
 
 class _AirConditioningScreenState extends State<AirConditioningScreen> {
-
+  List<dynamic> itemList = [
+    {
+      "id": 1,
+      "name": "heating",
+      "businessType": "Centropolis",
+      "type": "11F",
+      "dateTime": "2021.03.21 13:00",
+      "status": "Received"
+    },
+    {
+      "id": 2,
+      "name": "heating",
+      "businessType": "Centropolis",
+      "type": "11F",
+      "dateTime": "2021.03.21 13:00",
+      "status": "Received"
+    },
+    {
+      "id": 3,
+      "name": "Air conditioning",
+      "businessType": "Centropolis",
+      "type": "11F",
+      "dateTime": "2021.03.21 13:00",
+      "status": "Received"
+    }
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        body: Center(
-            child: Text(
-              tr("airConditioning"),
-              style: const TextStyle(
-                  fontFamily: 'SemiBold',
-                  fontSize: 20,
-                  color: CustomColors.textColor8),
-            )
-        ));
+        body: VocCommonHome(
+            image: 'assets/images/inconvenience_dummy_image.png',
+            title: 'Request for Heating and Cooling',
+            subTitle: 'Request for Heating and Cooling',
+            emptyTxt: "There is no cooling & heating application history.",
+            buttonName: tr("AirConditioning"),
+            itemsList: itemList,
+            onDrawerClick: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => AirIncLightList(
+                          emptyTxt: "There is no history of lights out.",
+                          itemsList: itemList,
+                        )),
+              );
+            },
+            onPressed: () {
+              debugPrint("===================================");
+            }));
   }
-
-
-
 }
