e3c3eb0309d04b00cd66b9a7b24531efc0d2a0e0
MondiraHanRiver <83229204+MondiraHanRiver@users.noreply.github.com>
2023-05-18 17:39:29 +0530

    code clean

--- centropolis/lib/utils/custom_urls.dart
+++ centropolis/lib/utils/custom_urls.dart
@@ -25,13 +25,7 @@ class ApiEndPoint {
   static String getLoungeHistoryListUrl = "${baseUrl}v1/mypage/executive-lounge-list";
   static String getConferenceHistoryListUrl = "${baseUrl}v1/mypage/conference-list";
 
-
-
-
-  static String getGxFitnessReservationUrl =
-      "${baseUrl}v1/amenity/gx-program-list";
-  static String getLoungeHistoryListUrl =
-      "${baseUrl}v1/mypage/executive-lounge-list";
+  
 
   static String getPtTimeListUrl = "${baseUrl}v1/common/pt-time";
   static String makePtReservation =
