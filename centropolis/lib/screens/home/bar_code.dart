9bf361cf3573e9a66b167e7602456ee21f83084e
MondiraHanRiver <83229204+MondiraHanRiver@users.noreply.github.com>
2023-06-19 13:24:32 +0530

    attached timer & reload the qr code

--- centropolis/lib/screens/home/bar_code.dart
+++ centropolis/lib/screens/home/bar_code.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'dart:convert';
+import 'package:flutter_svg/svg.dart';
 import 'package:http/http.dart' as http;
 import 'package:barcode_widget/barcode_widget.dart';
 import 'package:centropolis/widgets/common_button.dart';
@@ -15,9 +17,6 @@ import '../../utils/internet_checking.dart';
 import '../../utils/utils.dart';
 import '../../widgets/app_bar_for_dialog.dart';
 
-
-
-
 class BarCodeScreen extends StatefulWidget {
   const BarCodeScreen({super.key});
 
@@ -30,6 +29,14 @@ class _BarCodeScreenState extends State<BarCodeScreen> {
   late FToast fToast;
   bool isLoading = false;
   String qrCodeUrl = "";
+
+  Timer? timer;
+  static const minSeconds = 0;
+  int seconds = 300;
+  // int seconds = 20;
+  String showTime = "5:00";
+  bool isRunning = false;
+
 
 
   @override
@@ -44,8 +51,10 @@ class _BarCodeScreenState extends State<BarCodeScreen> {
     loadQrCode();
   }
 
+
   @override
   void dispose() {
+    timer?.cancel();
     super.dispose();
   }
 
@@ -59,92 +68,144 @@ class _BarCodeScreenState extends State<BarCodeScreen> {
       ),
       isLoading: isLoading,
       child: Scaffold(
-      backgroundColor: CustomColors.backgroundColor,
-      appBar: PreferredSize(
-        preferredSize: AppBar().preferredSize,
-        child: SafeArea(
+        backgroundColor: CustomColors.backgroundColor,
+        appBar: PreferredSize(
+          preferredSize: AppBar().preferredSize,
+          child: SafeArea(
+            child: Container(
+              color: CustomColors.whiteColor,
+              child: AppBarForDialog(tr("qrCheckIn"), () {
+                onBackButtonPress(context);
+              }),
+            ),
+          ),
+        ),
+        body: Center(
+            child: SingleChildScrollView(
           child: Container(
-            color: CustomColors.whiteColor,
-            child: AppBarForDialog(tr("qrCheckIn"), () {
-              onBackButtonPress(context);
-            }),
-          ),
-        ),
-      ),
-      body: Center(
-          child: SingleChildScrollView(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          margin: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: MediaQuery.of(context).size.width,
-                margin: const EdgeInsets.only(top: 90, bottom: 16),
-                child: Text(
-                  tr('barCodeTitle'),
-                  textAlign: TextAlign.center,
-                  maxLines: 1,
-                  style: const TextStyle(
-                      fontFamily: 'SemiBold',
-                      fontSize: 16,
-                      color: CustomColors.textColorBlack2),
-                ),
-              ),
-              Text(
-                tr('barCodeSubTitle'),
-                textAlign: TextAlign.center,
-                maxLines: 2,
-                style: const TextStyle(
-                    fontFamily: 'Regular',
-                    fontSize: 14,
-                    color: CustomColors.greyColor1),
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                    color: CustomColors.whiteColor,
-                    borderRadius: BorderRadius.circular(4)),
-                margin: const EdgeInsets.only(top: 32, bottom: 122),
-                padding: const EdgeInsets.symmetric(horizontal: 66, vertical: 32),
-                // child: BarcodeWidget(
-                //   barcode: Barcode.qrCode(),
-                //   data: 'Centropolis App QR code generator',
-                //   height: 240,
-                //   width: 210,
-                // ),
-                child: qrCodeUrl != "" ?
-                Image.network(
-                  '${ImageLinks.baseUrlForImage}$qrCodeUrl',
-                width: 200,height: 200,) : null,
-              ),
-              Container(
+            width: MediaQuery.of(context).size.width,
+            margin: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
                   width: MediaQuery.of(context).size.width,
-                  margin: const EdgeInsets.only(bottom: 40),
-                  child: CommonButton(
-                    isIconVisible: false,
-                    onCommonButtonTap: () {
-                      onBackButtonPress(context);
-                    },
-                    buttonColor: CustomColors.buttonBackgroundColor,
-                    buttonName: tr("check"),
-                  ))
-            ],
+                  margin: const EdgeInsets.only(top: 90, bottom: 16),
+                  child: Text(
+                    tr('barCodeTitle'),
+                    textAlign: TextAlign.center,
+                    maxLines: 1,
+                    style: const TextStyle(
+                        fontFamily: 'SemiBold',
+                        fontSize: 16,
+                        color: CustomColors.textColorBlack2),
+                  ),
+                ),
+                Text(
+                  tr('barCodeSubTitle'),
+                  textAlign: TextAlign.center,
+                  maxLines: 2,
+                  style: const TextStyle(
+                      fontFamily: 'Regular',
+                      fontSize: 14,
+                      color: CustomColors.greyColor1),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      color: CustomColors.whiteColor,
+                      borderRadius: BorderRadius.circular(4)),
+                  margin: const EdgeInsets.only(top: 32, bottom: 122),
+                  padding: const EdgeInsets.symmetric(horizontal: 66, vertical: 32),
+                  // child: BarcodeWidget(
+                  //   barcode: Barcode.qrCode(),
+                  //   data: 'Centropolis App QR code generator',
+                  //   height: 240,
+                  //   width: 210,
+                  // ),
+                  child: Column(
+                    children: [
+                       Row(
+                         mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SvgPicture.asset(
+                            'assets/images/ic_date.svg',
+                            semanticsLabel: 'Back',
+                            width: 15,
+                            height: 15,
+                            alignment: Alignment.center,
+                          ),
+
+                          const SizedBox(width: 4,),
+
+                          const Text(
+                            "Timmer ",
+                            textAlign: TextAlign.center,
+                            maxLines: 2,
+                            style: TextStyle(
+                                fontFamily: 'Regular',
+                                fontSize: 14,
+                                color: CustomColors.textColor3),
+                          ),
+
+
+                           Text(
+                            // "02 min",
+                            showTime,
+                            textAlign: TextAlign.center,
+                            maxLines: 2,
+                            style: TextStyle(
+                                fontFamily: 'Medium',
+                                fontSize: 14,
+                                color: CustomColors.textColor9),
+                          ),
+
+                        ],
+                      ),
+
+
+
+
+                      SizedBox(
+                        child:  qrCodeUrl != "" ?
+                        Image.network(
+                          '${ImageLinks.baseUrlForImage}$qrCodeUrl',
+                          width: 200, height: 200,) : null,
+                      )
+
+
+
+
+                    ],
+                  ),
+                ),
+                Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: const EdgeInsets.only(bottom: 40),
+                    child: CommonButton(
+                      isIconVisible: false,
+                      onCommonButtonTap: () {
+                        onBackButtonPress(context);
+                      },
+                      buttonColor: CustomColors.buttonBackgroundColor,
+                      buttonName: tr("check"),
+                    ))
+              ],
+            ),
           ),
-        ),
-      )),
-    ),);
+        )),
+      ),
+    );
   }
 
-  void loadQrCode() async{
+  void loadQrCode() async {
     final InternetChecking internetChecking = InternetChecking();
     if (await internetChecking.isInternet()) {
       callLoadQrCodeApi();
     } else {
-    showCustomToast(fToast, context, tr("noInternetConnection"), "");
+      showCustomToast(fToast, context, tr("noInternetConnection"), "");
     }
   }
 
@@ -157,25 +218,20 @@ class _BarCodeScreenState extends State<BarCodeScreen> {
     debugPrint("Get QR Code input===> $body");
 
     Future<http.Response> response = WebService().callPostMethodWithRawData(
-        ApiEndPoint.getQrCodeUrl,
-        body,
-        language.toString(),
-        apiKey);
+        ApiEndPoint.getQrCodeUrl, body, language.toString(), apiKey);
     response.then((response) {
       var responseJson = json.decode(response.body);
 
-      debugPrint(
-          "server response for Get QR Code ===> $responseJson");
+      debugPrint("server response for Get QR Code ===> $responseJson");
 
       if (responseJson != null) {
         if (response.statusCode == 200 && responseJson['success']) {
-
-          if(responseJson['qr_code_path'] != null){
+          if (responseJson['qr_code_path'] != null) {
             setState(() {
               qrCodeUrl = responseJson['qr_code_path'].toString();
             });
+            startTimer();
           }
-
         } else {
           if (responseJson['message'] != null) {
             showCustomToast(
@@ -195,6 +251,59 @@ class _BarCodeScreenState extends State<BarCodeScreen> {
   }
 
 
+  void startTimer() {
+    if (seconds == minSeconds) {
+      resetTimer();
+    }
+
+    const oneSec = Duration(seconds: 1);
+    timer = Timer.periodic(
+      oneSec,
+          (Timer timer) {
+        if (seconds == 0) {
+          stopTimer(true);
+          setState(() {
+            showTime = "";
+          });
+          loadQrCode();
+        }
+        else {
+          setState(() {
+            seconds--;
+          });
+
+          int min = seconds ~/ 60;
+          int sec = seconds % 60;
+          String parsedTime = "$min : ${getParsedTime(sec.toString())}";
+          setState(() {
+            showTime = parsedTime;
+          });
+        }
+      },
+    );
+  }
+
+  String getParsedTime(String time) {
+    if (time.length <= 1) return "0$time";
+    return time;
+  }
+
+  void stopTimer(bool reset) {
+    if (reset) {
+      resetTimer();
+    }
+
+    setState(() {
+      timer?.cancel();
+    });
+  }
+
+  void resetTimer() {
+    setState(() {
+      seconds = 300;
+      // seconds = 20;
+    });
+  }
 
 
 }
