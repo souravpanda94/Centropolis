13f9c70fba6672167a17e1115ba346365ebe4f29
Aditi Dande <aditi@divii.com>
2023-08-18 10:38:01 +0530

    conference schedule UI changes

--- centropolis/lib/screens/amenity/conference_availability_modal.dart
+++ centropolis/lib/screens/amenity/conference_availability_modal.dart
@@ -42,128 +42,184 @@ class _ConferenceAvailabilityModalState
         borderRadius: BorderRadius.circular(4),
       ),
       insetPadding: const EdgeInsets.only(
-        left: 10.0,
-        right: 10.0,
-        top: 30,
+        left: 16.0,
+        right: 16.0,
+        top: 20,
         bottom: 30.0,
       ),
-
       child: Container(
         width: double.infinity,
-        padding: const EdgeInsets.only(left: 12, right: 12, bottom: 16),
-        child: TableCalendar(
-          shouldFillViewport: true,
-          availableGestures: AvailableGestures.horizontalSwipe,
-          locale: Localizations.localeOf(context).languageCode,
-          rangeSelectionMode: RangeSelectionMode.toggledOff,
-          availableCalendarFormats: const {CalendarFormat.month: 'Month'},
-          weekendDays: const [DateTime.saturday, DateTime.sunday],
-          daysOfWeekHeight: 40,
-          focusedDay: focusedDate,
-          calendarFormat: selectedCalendarFormat,
-          firstDay: kFirstDay,
-          lastDay: kLastDay,
-          headerStyle: HeaderStyle(
-              headerPadding: EdgeInsets.only(
-                  left: 0,
-                  right: 0,
-                  bottom: 8,
-                  top: Platform.isAndroid ? 16 : 8),
-              formatButtonVisible: false,
-              titleCentered: false,
-              titleTextStyle: const TextStyle(
-                  fontFamily: 'SemiBold', fontSize: 16, color: Colors.black),
-              titleTextFormatter: (date, locale) {
-                return "${DateFormat.MMMM(locale).format(date)} ${DateFormat.y(locale).format(date)}";
-              },
-              leftChevronVisible: false,
-              rightChevronVisible: false),
-          daysOfWeekStyle: DaysOfWeekStyle(
-              dowTextFormatter: (date, locale) =>
-                  DateFormat.E(locale).format(date),
-              weekdayStyle: const TextStyle(
-                color: CustomColors.blackColor,
-                fontFamily: 'Regular',
-                fontSize: 14,
-              ),
-              weekendStyle: const TextStyle(
-                color: CustomColors.blackColor,
-                fontFamily: 'Regular',
-                fontSize: 14,
-              ),
-              decoration: const BoxDecoration(
-                  color: CustomColors.greyColor2,
-                  border: Border(
-                    bottom:
-                        BorderSide(width: 1, color: CustomColors.borderColor),
-                  ))),
-          calendarStyle: const CalendarStyle(
-              todayTextStyle: TextStyle(color: Colors.black),
-              weekendTextStyle: TextStyle(color: Color(0xffCC6047)),
-              disabledTextStyle: TextStyle(color: Colors.grey),
-              disabledDecoration:
-                  BoxDecoration(color: Colors.white, shape: BoxShape.circle),
-              todayDecoration: BoxDecoration(
-                  color: CustomColors.backgroundColor3, shape: BoxShape.circle),
-              selectedTextStyle: TextStyle(color: Colors.white),
-              selectedDecoration: BoxDecoration(
-                  color: Color(0xffCC6047), shape: BoxShape.circle),
-              defaultTextStyle: TextStyle(
-                fontFamily: 'Regular',
-                fontSize: 14,
-              )),
-          selectedDayPredicate: (day) {
-            // if (isSameDay(day, focusedDate)) {
-            //   return true;
-            // } else {
-            //   return false;
-            // }
-            return false;
-          },
-          enabledDayPredicate: (day) {
-            // if (day.weekday == DateTime.saturday ||
-            //     day.weekday == DateTime.sunday) {
-            //   return false;
-            // } else if (day.day == kFirstDay.day &&
-            //     day.month == kFirstDay.month &&
-            //     day.year == kFirstDay.year) {
-            //   return true;
-            // } else if (day.compareTo(kFirstDay) > 0) {
-            //   return true;
-            // } else {
-            //   return false;
-            // }
-            return true;
-          },
-          onFormatChanged: (format) {
-            if (selectedCalendarFormat != format) {
-              setState(() {
-                selectedCalendarFormat = format;
-              });
-            }
-          },
-          calendarBuilders: CalendarBuilders(
-            markerBuilder: (context, day, focusedDay) {
-              var stringDate =
-                  "${day.year}-${day.month.toString().length == 1 ? "0" : ""}${day.month}-${day.day.toString().length == 1 ? "0" : ""}${day.day}";
-              List<dynamic> eventList = widget.scheduleList[stringDate] ?? [];
-              return Container(
-                  width: 150,
-                  height: 140,
-                  padding: const EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    color: day.day.compareTo(DateTime.now().day) == 0
-                        ? CustomColors.backgroundColor3
-                        : CustomColors.whiteColor,
-                    border: Border.all(
-                      width: 0.5,
-                      color: CustomColors.borderColor,
+        padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
+        child: ListView(
+          shrinkWrap: true,
+          scrollDirection: Axis.horizontal,
+          children: [
+            SizedBox(
+              width: 1000,
+              child: TableCalendar(
+                shouldFillViewport: true,
+                //availableGestures: AvailableGestures.horizontalSwipe,
+                locale: Localizations.localeOf(context).languageCode,
+                rangeSelectionMode: RangeSelectionMode.toggledOff,
+                availableCalendarFormats: const {CalendarFormat.month: 'Month'},
+                weekendDays: const [DateTime.saturday, DateTime.sunday],
+                daysOfWeekHeight: 40,
+                focusedDay: focusedDate,
+                calendarFormat: selectedCalendarFormat,
+                firstDay: kFirstDay,
+                lastDay: kLastDay,
+                headerStyle: HeaderStyle(
+                    headerPadding: EdgeInsets.only(
+                        left: 0,
+                        right: 0,
+                        bottom: 8,
+                        top: Platform.isAndroid ? 16 : 8),
+                    formatButtonVisible: false,
+                    titleCentered: false,
+                    titleTextStyle: const TextStyle(
+                        fontFamily: 'SemiBold',
+                        fontSize: 16,
+                        color: Colors.black),
+                    titleTextFormatter: (date, locale) {
+                      return "${DateFormat.MMMM(locale).format(date)} ${DateFormat.y(locale).format(date)}";
+                    },
+                    leftChevronVisible: false,
+                    rightChevronVisible: false),
+                daysOfWeekStyle: DaysOfWeekStyle(
+                    dowTextFormatter: (date, locale) =>
+                        DateFormat.E(locale).format(date),
+                    weekdayStyle: const TextStyle(
+                      color: CustomColors.blackColor,
+                      fontFamily: 'Regular',
+                      fontSize: 14,
                     ),
-                  ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      SizedBox(
+                    weekendStyle: const TextStyle(
+                      color: CustomColors.blackColor,
+                      fontFamily: 'Regular',
+                      fontSize: 14,
+                    ),
+                    decoration: const BoxDecoration(
+                        color: CustomColors.greyColor2,
+                        border: Border(
+                          bottom: BorderSide(
+                              width: 1, color: CustomColors.borderColor),
+                        ))),
+                calendarStyle: const CalendarStyle(
+                    todayTextStyle: TextStyle(color: Colors.black),
+                    weekendTextStyle: TextStyle(color: Color(0xffCC6047)),
+                    disabledTextStyle: TextStyle(color: Colors.grey),
+                    disabledDecoration: BoxDecoration(
+                        color: Colors.white, shape: BoxShape.circle),
+                    todayDecoration: BoxDecoration(
+                        color: CustomColors.backgroundColor3,
+                        shape: BoxShape.circle),
+                    selectedTextStyle: TextStyle(color: Colors.white),
+                    selectedDecoration: BoxDecoration(
+                        color: Color(0xffCC6047), shape: BoxShape.circle),
+                    defaultTextStyle: TextStyle(
+                      fontFamily: 'Regular',
+                      fontSize: 14,
+                    )),
+                selectedDayPredicate: (day) {
+                  return false;
+                },
+                enabledDayPredicate: (day) {
+                  return true;
+                },
+                onFormatChanged: (format) {
+                  if (selectedCalendarFormat != format) {
+                    setState(() {
+                      selectedCalendarFormat = format;
+                    });
+                  }
+                },
+                calendarBuilders: CalendarBuilders(
+                  markerBuilder: (context, day, focusedDay) {
+                    var stringDate =
+                        "${day.year}-${day.month.toString().length == 1 ? "0" : ""}${day.month}-${day.day.toString().length == 1 ? "0" : ""}${day.day}";
+                    List<dynamic> eventList =
+                        widget.scheduleList[stringDate] ?? [];
+                    return Container(
+                        width: 150,
+                        height: 200,
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: day.day.compareTo(DateTime.now().day) == 0
+                                ? CustomColors.backgroundColor3
+                                : CustomColors.whiteColor,
+                            border: const Border(
+                              top: BorderSide(
+                                  width: 0.5, color: CustomColors.borderColor),
+                              bottom: BorderSide(
+                                  width: 0.5, color: CustomColors.borderColor),
+                              right: BorderSide(
+                                  width: 1, color: CustomColors.borderColor),
+                            )),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            SizedBox(
+                              width: double.infinity,
+                              child: Text(
+                                  getOrdinalDay(
+                                      int.parse(DateFormat.d().format(day)),
+                                      language),
+                                  textAlign: TextAlign.end,
+                                  style: const TextStyle(
+                                    color: CustomColors.textColorBlack2,
+                                    fontFamily: 'Regular',
+                                    fontSize: 12,
+                                  )),
+                            ),
+                            const SizedBox(
+                              height: 8,
+                            ),
+                            if (eventList.isNotEmpty)
+                              Expanded(
+                                child: ListView.separated(
+                                    shrinkWrap: true,
+                                    scrollDirection: Axis.vertical,
+                                    itemBuilder: (context, index) {
+                                      return SizedBox(
+                                        width: double.infinity,
+                                        child: Text(
+                                            "${DateFormat('hh:mm a').format(DateTime.parse("$stringDate ${eventList[index]["start_time"]}"))}  ${eventList[index]["conference_hall"]}",
+                                            textAlign: TextAlign.start,
+                                            style: const TextStyle(
+                                                color: CustomColors
+                                                    .textColorBlack2,
+                                                fontFamily: 'Regular',
+                                                fontSize: 12,
+                                                height: 1.5)),
+                                      );
+                                    },
+                                    separatorBuilder: (context, index) {
+                                      return const SizedBox(
+                                        height: 8,
+                                      );
+                                    },
+                                    itemCount: eventList.length),
+                              )
+                          ],
+                        ));
+                  },
+                  disabledBuilder: (context, day, focusedDay) {
+                    return Container(
+                      width: 150,
+                      height: 200,
+                      padding: const EdgeInsets.all(10),
+                      decoration: const BoxDecoration(
+                        color: CustomColors.whiteColor,
+                        border: Border(
+                          top: BorderSide(
+                              width: 0.5, color: CustomColors.borderColor),
+                          bottom: BorderSide(
+                              width: 0.5, color: CustomColors.borderColor),
+                          right: BorderSide(
+                              width: 1, color: CustomColors.borderColor),
+                        ),
+                      ),
+                      child: SizedBox(
                         width: double.infinity,
                         child: Text(
                             getOrdinalDay(int.parse(DateFormat.d().format(day)),
@@ -175,93 +231,14 @@ class _ConferenceAvailabilityModalState
                               fontSize: 12,
                             )),
                       ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      if (eventList.isNotEmpty)
-                        Expanded(
-                          child: ListView.separated(
-                              shrinkWrap: true,
-                              scrollDirection: Axis.vertical,
-                              itemBuilder: (context, index) {
-                                return SizedBox(
-                                  width: double.infinity,
-                                  child: Text(
-                                      "${DateFormat('hh:mm a').format(DateTime.parse("$stringDate ${eventList[index]["start_time"]}"))}  ${eventList[index]["conference_hall"]}",
-                                      textAlign: TextAlign.start,
-                                      style: const TextStyle(
-                                        color: CustomColors.textColorBlack2,
-                                        fontFamily: 'Regular',
-                                        fontSize: 12,
-                                      )),
-                                );
-                              },
-                              separatorBuilder: (context, index) {
-                                return const SizedBox(
-                                  height: 8,
-                                );
-                              },
-                              itemCount: eventList.length),
-                        )
-                    ],
-                  ));
-            },
-            disabledBuilder: (context, day, focusedDay) {
-              return Container(
-                width: 150,
-                height: 140,
-                padding: const EdgeInsets.all(5),
-                decoration: BoxDecoration(
-                  color: CustomColors.whiteColor,
-                  border: Border.all(
-                    width: 0.5,
-                    color: CustomColors.borderColor,
-                  ),
+                    );
+                  },
                 ),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: Text(
-                      getOrdinalDay(
-                          int.parse(DateFormat.d().format(day)), language),
-                      textAlign: TextAlign.end,
-                      style: const TextStyle(
-                        color: CustomColors.textColorBlack2,
-                        fontFamily: 'Regular',
-                        fontSize: 12,
-                      )),
-                ),
-              );
-            },
-          ),
+              ),
+            )
+          ],
         ),
       ),
-      // content: MonthView(
-      //   showBorder: true,
-      //   headerBuilder: (date) {
-      //     return Padding(
-      //       padding: const EdgeInsets.only(bottom: 8),
-      //       child: Text(DateFormat.yMMMM().format(DateTime.now())),
-      //     );
-      //   },
-      //   headerStyle: const HeaderStyle(
-      //       leftIconVisible: false,
-      //       rightIconVisible: false,
-      //       leftIconPadding: EdgeInsets.all(0),
-      //       rightIconPadding: EdgeInsets.all(0),
-      //       titleAlign: TextAlign.start,
-      //       headerPadding: EdgeInsets.only(bottom: 8),
-      //       headerTextStyle: TextStyle(
-      //           fontFamily: 'SemiBold',
-      //           fontSize: 16,
-      //           color: CustomColors.textColor8),
-      //       decoration: BoxDecoration(color: Colors.red)),
-      //   weekDayBuilder: (day) {
-      //     return Container(
-      //         padding: const EdgeInsets.all(8),
-      //         color: CustomColors.greyColor2,
-      //         child: Text(day == 0 ? "Mon" : "Tue"));
-      //   },
-      // ),
     );
   }
 }
