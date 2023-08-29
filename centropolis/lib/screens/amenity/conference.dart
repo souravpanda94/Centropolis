43b09294711d019428df67d73b6aad34670f13ac
PradiptaHanriver <pradipta@divii.com>
2023-08-29 18:53:21 +0530

    conference reservation modal attached

--- centropolis/lib/screens/amenity/conference.dart
+++ centropolis/lib/screens/amenity/conference.dart
@@ -209,7 +209,12 @@ class _ConferenceScreenState extends State<ConferenceScreen> {
     if (await internetChecking.isInternet()) {
       callLoadPersonalInformationApi();
     } else {
-      showCustomToast(fToast, context, tr("noInternetConnection"), "");
+      //showCustomToast(fToast, context, tr("noInternetConnection"), "");
+       showErrorCommonModal(
+          context: context,
+          heading: tr("noInternet"),
+          description: tr("connectionFailedDescription"),
+          buttonName: tr("check"));
     }
   }
 
@@ -238,8 +243,13 @@ class _ConferenceScreenState extends State<ConferenceScreen> {
           });
         } else {
           if (responseJson['message'] != null) {
-            showCustomToast(
-                fToast, context, responseJson['message'].toString(), "");
+            debugPrint("Server error response ${responseJson['message']}");
+              // showCustomToast(
+              //     fToast, context, responseJson['message'].toString(), "");
+              showErrorCommonModal(context: context,
+                  heading :responseJson['message'].toString(),
+                  description: "",
+                  buttonName: tr("check"));
           }
         }
       }
@@ -248,6 +258,10 @@ class _ConferenceScreenState extends State<ConferenceScreen> {
       });
     }).catchError((onError) {
       debugPrint("catchError ================> $onError");
+      showErrorCommonModal(context: context,
+          heading: tr("errorDescription"),
+          description:"",
+          buttonName : tr("check"));
       setState(() {
         isLoading = false;
       });
