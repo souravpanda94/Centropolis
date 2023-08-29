60d1926de6eeb6e4ca0306c4c10eab7f03e9de29
PradiptaHanriver <pradipta@divii.com>
2023-08-29 18:01:35 +0530

    gx detail modal attached

--- centropolis/lib/screens/amenity/gx_reservation_detail.dart
+++ centropolis/lib/screens/amenity/gx_reservation_detail.dart
@@ -524,7 +524,12 @@ class _GXReservationDetailState extends State<GXReservationDetail> {
     if (await internetChecking.isInternet()) {
       callReservationApi();
     } else {
-      showCustomToast(fToast, context, tr("noInternetConnection"), "");
+      //showCustomToast(fToast, context, tr("noInternetConnection"), "");
+         showErrorCommonModal(
+          context: context,
+          heading: tr("noInternet"),
+          description: tr("connectionFailedDescription"),
+          buttonName: tr("check"));
     }
   }
 
@@ -558,8 +563,13 @@ class _GXReservationDetailState extends State<GXReservationDetail> {
           }
         } else {
           if (responseJson['message'] != null) {
-            showCustomToast(
-                fToast, context, responseJson['message'].toString(), "");
+            debugPrint("Server error response ${responseJson['message']}");
+              // showCustomToast(
+              //     fToast, context, responseJson['message'].toString(), "");
+              showErrorCommonModal(context: context,
+                  heading :responseJson['message'].toString(),
+                  description: "",
+                  buttonName: tr("check"));
           }
         }
         setState(() {
@@ -568,6 +578,10 @@ class _GXReservationDetailState extends State<GXReservationDetail> {
       }
     }).catchError((onError) {
       debugPrint("catchError ================> $onError");
+       showErrorCommonModal(context: context,
+          heading: tr("errorDescription"),
+          description:"",
+          buttonName : tr("check"));
       setState(() {
         isLoading = false;
       });
