90e0e0a49ebb116824c68a4a4d8a5c44bb3c8539
PradiptaHanriver <pradipta@divii.com>
2023-09-04 17:40:10 +0530

    catch changes sleeping room

--- centropolis/lib/screens/amenity/sleeping_room_reservation.dart
+++ centropolis/lib/screens/amenity/sleeping_room_reservation.dart
@@ -1206,13 +1206,17 @@ class _SleepingRoomReservationState extends State<SleepingRoomReservation> {
       }
     }).catchError((onError) {
       debugPrint("catchError ================> $onError");
-       showErrorCommonModal(context: context,
+      if(mounted){
+        showErrorCommonModal(context: context,
           heading: tr("errorDescription"),
           description:"",
           buttonName : tr("check"));
       setState(() {
         isLoading = false;
       });
+
+      }
+       
     });
   }
 
